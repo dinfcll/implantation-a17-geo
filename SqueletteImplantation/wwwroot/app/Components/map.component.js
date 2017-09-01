4f0defe1b0ad6ae8b4fba7e678070f603cfb54f9
annie deshaies <annie.deshaies@gmail.com>
2017-09-01 12:42:00 -0400

    étape d'exécution pour quand on exécute le projet pour la première fois

--- SqueletteImplantation/wwwroot/app/Components/map.component.js
+++ SqueletteImplantation/wwwroot/app/Components/map.component.js
@@ -5,17 +5,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var MapComponent = (function () {
+var MapComponent = /** @class */ (function () {
     function MapComponent() {
         this.name = 'Map';
     }
+    MapComponent = __decorate([
+        core_1.Component({
+            selector: 'my-map',
+        })
+    ], MapComponent);
     return MapComponent;
 }());
-MapComponent = __decorate([
-    core_1.Component({
-        selector: 'my-map',
-    })
-], MapComponent);
 exports.MapComponent = MapComponent;
 //# sourceMappingURL=map.component.js.map
--- SqueletteImplantation/wwwroot/app/app.component.js
+++ SqueletteImplantation/wwwroot/app/app.component.js
@@ -5,19 +5,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var AppComponent = (function () {
+var AppComponent = /** @class */ (function () {
     function AppComponent() {
         this.name = 'Angular';
     }
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            templateUrl: './view/test.html',
+            styleUrls: ['./../css/styles.css', './../css/font-awesome.min.css', './../css/bootstrap.css']
+        })
+    ], AppComponent);
     return AppComponent;
 }());
-AppComponent = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        templateUrl: './view/test.html',
-        styleUrls: ['./../css/styles.css', './../css/font-awesome.min.css', './../css/bootstrap.css']
-    })
-], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
