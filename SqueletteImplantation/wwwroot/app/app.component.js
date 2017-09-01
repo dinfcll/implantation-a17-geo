bebd9d3409d75a6fa285ce75c34536cf0629b75e
Antoine Emond <antoine.emond@hotmail.com>
2017-09-01 13:39:45 -0400

    font-awesome WORKS !

--- SqueletteImplantation/wwwroot/app/app.component.js
+++ SqueletteImplantation/wwwroot/app/app.component.js
@@ -5,7 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
@@ -17,7 +16,7 @@ AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
         templateUrl: './view/test.html',
-        styleUrls: ['./../css/styles.css', './../css/font-awesome.min.css', './../css/bootstrap.css']
+        styleUrls: ['./../css/styles.css', './../lib/font-awesome-4.7.0/css/font-awesome.css', './../lib/bootstrap/dist/css/bootstrap.min.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
