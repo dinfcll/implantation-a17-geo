c9b8fa4c93c5ae89d005e7b624c5a46f169b8301
annie deshaies <annie.deshaies@gmail.com>
2017-09-01 12:24:41 -0400

    me suit trompé de branche

--- SqueletteImplantation/DbEntities/Models/Utilisateur.cs
+++
@@ -1,9 +0,0 @@
-namespace SqueletteImplantation.DbEntities.Models
-{
-    public class Utilisateur
-    {
-        public int Id { get; set; }
-        public string email { get; set; }
-        public string mdp { get; set; }
-    }
-}
