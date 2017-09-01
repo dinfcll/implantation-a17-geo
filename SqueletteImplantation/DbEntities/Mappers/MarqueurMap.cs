a41b23aa123017b480b869f8722fa927b9da7fab
Antoine Emond <antoine.emond@hotmail.com>
2017-09-01 11:32:37 -0400

    MarquerMigration

---
+++ SqueletteImplantation/DbEntities/Mappers/MarqueurMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SqueletteImplantation.DbEntities.Models;
+
+namespace SqueletteImplantation.DbEntities.Mappers
+{
+    public class MarqueurMap
+    {
+        public MarqueurMap(EntityTypeBuilder<Marqueur> entityBuilder)
+        {
+            entityBuilder.HasKey(m => m.Id);
+            entityBuilder.Property(m => m.Nom).IsRequired();
+            entityBuilder.Property(m => m.Longitude).IsRequired();
+            entityBuilder.Property(m => m.Latitude).IsRequired();
+        }
+    }
+}
+
---
+++ SqueletteImplantation/Migrations/20170901143215_AjoutMarqueur2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace squeletteimplantation.Migrations
+{
+    public partial class AjoutMarqueur2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marqueur",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Latitude = table.Column<decimal>(nullable: false),
+                    Longitude = table.Column<decimal>(nullable: false),
+                    Nom = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marqueur", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marqueur");
+        }
+    }
+}
