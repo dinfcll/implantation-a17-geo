dd60ba32719a72a9129133e58b56bc2184728d80
Pascal Audet <pascal-audet@hotmail.com>
2017-09-18 17:30:45 -0400

    nouveaux fichiers profil-utilisateur

---
+++ SqueletteImplantation/wwwroot/app/Components/profil-utilisateur/profil-utilisateur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { ProfilUtilisateur } from './../../class/profilutilisateur.class';
+
+@Component({
+    selector: 'profil-utilisateur',
+    templateUrl: './profil-utilisateur.component.html'
+})
+
+export class ProfilUtilisateurComponent {
+
+}
---
+++ SqueletteImplantation/wwwroot/app/class/profilutilisateur.class.ts
@@ -0,0 +1,5 @@
+export class ProfilUtilisateur {
+    constructor(public id : number, public courriel : string, public username : string, public prenom : string, public nom : string) {
+
+    }
+}
