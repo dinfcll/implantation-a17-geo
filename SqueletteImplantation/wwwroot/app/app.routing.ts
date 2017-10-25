e29fe2dcb0dcd4fd56a2522b6c14759cef764bae
Pascal Audet <pascal-audet@hotmail.com>
2017-10-25 17:49:31 -0400

    Update app.routing.ts ENTER EN TROP

--- SqueletteImplantation/wwwroot/app/app.routing.ts
+++ SqueletteImplantation/wwwroot/app/app.routing.ts
@@ -12,8 +12,6 @@ import { PostUserComponent } from './Components/post/post.component';
 import { ProfilUtilisateurComponent } from './Components/profil-utilisateur/profil-utilisateur.component';
 import { ResetPWComponent } from './Components/password-reset/resetPW.component';
 
-
-
 export const routing: Routes = [
     {
         path: '',
