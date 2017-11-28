e488be822d4cf38a03f46510b38f839947fe5416
Jérôme Labrie <jeromelab@hotmail.fr>
2017-11-27 19:02:03 -0500

    ajout des gifs et début création présentation

--- SqueletteImplantation/wwwroot/app/Components/apropos/apropos.component.ts
+++ SqueletteImplantation/wwwroot/app/Components/apropos/apropos.component.ts
@@ -6,10 +6,24 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'aPropos',
     templateUrl: './apropos.component.html',
-    styleUrls: ['./apropos.component.css']
+    styleUrls: ['./apropos.component.css','./../../../lib/bootstrap/dist/css/bootstrap.css']
 })
 
 export class aProposComponent {
     constructor() { };
+
+    showDesc(){
+        document.getElementById('descriptionPerso').style.width = "100%";
+        let image = document.createElement("img");
+        image.src = "../../../images/annifire.gif";
+        image.style.maxHeight = "100%";
+        image.id = 'ImageSelf';
+        document.getElementById('PresentationImage').appendChild(image);
+    }
+
+    fermeDesc(){
+        document.getElementById('descriptionPerso').style.width = "0%";
+        document.getElementById('ImageSelf').remove();
+    }
     
 }
